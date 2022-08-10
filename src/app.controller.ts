1fa8ad1ab867c14cd4df714bfed2e277bd644481
keleris32 <charles.ejiegbu@gmail.com>
2022-08-10 04:19:26 +0100

    feat: added localAuthGuard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any {
+    return req.user;
+  }
 
   @Get()
   getHello(): string {
